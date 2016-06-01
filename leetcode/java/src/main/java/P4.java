e5e38c073c3474d8033b2464417b6b085043094b
zhanglong <zhanglong@staff.weibo.com>
2016-06-01 21:22:44 +0800

    fix

--- leetcode/java/src/main/java/P4.java
+++ leetcode/java/src/main/java/P4.java
@@ -3,25 +3,22 @@
  */
 public class P4 {
 
-    private static int kth(int a[], int lx, int hx, int b[], int ly, int hy, int k) {
-        if (hx - lx + 1 > hy - ly + 1) return kth(b, ly, hy, a, lx, hx, k);
-        if (hx - lx + 1 == 0) return b[k - 1];
+    private static int kth(int a[], int s1, int len1, int b[], int s2, int len2, int k) {
+        if (len1 > len2) return kth(b, s2, len2, a, s1, len1, k);
+        if (len1 == 0) return b[k - 1];
         if (k == 1) return Math.min(a[0], b[0]);
-        if (k == 2 && hx - lx == 0 && hy - ly == 0) return Math.max(a[0], b[0]);
-        int i = Math.min(k / 2, hx - lx + 1);
-        int j = Math.min(k / 2, hy - ly + 1);
-        if (a[i - 1] > b[j - 1]) {
-            return kth(a, lx, i - 1, b, j + 1, hy, k - j);
-        } else {
-            return kth(a, i + 1, hx, b, ly, j - 1, k - i);
-        }
+        if (k == 2 && len1 == 1 && len2 == 1) return Math.max(a[0], b[0]);
+        int i = Math.min(k / 2, len1);
+        int j = Math.min(k / 2, len2);
+        if (a[i - 1] > b[j - 1]) return kth(a, s1, len1 - i, b, s2 + j, len2 - j, k - j);
+        else return kth(a, s1 + i, len1 - i, b, s2, len2 - j, k - i);
     }
 
     public static void main(String[] args) {
         int x[] = new int[]{1,3};
         int y[] = new int[]{2};
         for (int i = 1; i <= x.length + y.length; i++) {
-            int value = kth(x, 1, x.length, y, 1, y.length, i);
+            int value = kth(x, 0, x.length, y, 0, y.length, i);
             System.out.println(String.format("value=%d", value));
         }
     }
