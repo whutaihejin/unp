5aeacc4db73c777876db9a6c50a93b58a2ce985f
taihejin <taihejin2008@126.com>
2016-07-16 13:50:37 +0800

    concurrent connect, still hava zombie child process

--- chapter5/client.c
+++ chapter5/client.c
@@ -10,24 +10,25 @@
 
 int main(int argc, char* argv[]) {
   struct sockaddr_in servaddr;
-  int sockfd;
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0) {
-    exit_print("socket error.\n");
-  }
-  memset((void*)&servaddr, 0, sizeof(servaddr));
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_port = htons(9999);
-  inet_aton("127.0.0.1", &servaddr.sin_addr);
+  int i, sockfd[5];
+  for (i = 0; i < 5; i++) {
+    if ((sockfd[i] = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+      exit_print("socket error.\n");
+    }
+    memset((void*)&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(9999);
+    inet_aton("127.0.0.1", &servaddr.sin_addr);
 
-  connect(sockfd, (const struct sockaddr*)&servaddr, sizeof(servaddr));
+    connect(sockfd[i], (const struct sockaddr*)&servaddr, sizeof(servaddr));
+  }
   
   char sendbuf[MAX_SIZE];
   char recvbuf[MAX_SIZE];
   ssize_t len;
   while (fgets(sendbuf, sizeof(sendbuf), stdin) != NULL) {
-    write(sockfd, sendbuf, strlen(sendbuf));
-    if ((len = read(sockfd, recvbuf, sizeof(recvbuf))) <= 0) {
+    write(sockfd[0], sendbuf, strlen(sendbuf));
+    if ((len = read(sockfd[0], recvbuf, sizeof(recvbuf))) <= 0) {
       continue;
     }
     recvbuf[len] = '\0';
